6a5fadfa0a8d14a8f94a995712d7e290ceff5444
jqb <kuba.janoszek@gmail.com>
2012-09-27 23:58:29 +0200

    Ubuntu conditional x-configuration

--- xemacs-only.el
+++ xemacs-only.el
@@ -1,9 +1,5 @@
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; for xemacs:
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(inhibit-startup-echo-area-message nil)
  '(inhibit-startup-screen t)
@@ -13,28 +9,8 @@
 (custom-set-faces
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(cursor ((t (:background "white")))))
-
-
-(defun maximize-screen (&optional f)
-       (interactive)
-       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-               '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-       (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-               '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
-(global-set-key [f11] 'maximize-screen)
-
-
-(defun full-screen (&optional f)
-  (interactive)
-  (set-frame-parameter f 'fullscreen
-		       (if (frame-parameter f 'fullscreen) nil 'fullboth)))
-(global-set-key [C-f11] 'full-screen)
-
-
 (add-to-list 'default-frame-alist '(alpha 90 90))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; end of xemacs settings
-
-
 
 
 ;; FOR WINDOWS
@@ -66,9 +42,22 @@
     (custom-set-faces
      '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "unknown" :family "Courier"))))
      '(cursor ((t (:background "white")))))
-
   )
 )
 
-(if (and window-system (not (eq system-type 'darwin)))
-   (maximize-screen))
+
+;; FOR Ubuntu
+(if (eq system-type 'gnu/linux)
+    (progn
+      (defun maximize-screen (&optional f)
+        (interactive)
+        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                               '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                               '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+      (defun full-screen (&optional f)
+        (interactive)
+        (set-frame-parameter f 'fullscreen (if (frame-parameter f 'fullscreen) nil 'fullboth)))
+      (global-set-key [f11] 'maximize-screen)
+      (global-set-key [C-f11] 'full-screen)
+      (maximize-screen)))
