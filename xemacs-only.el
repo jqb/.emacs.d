bb655f2f4c687eebb9b09d074c906c5debb8bd1e
jqb <kuba.janoszek@gmail.com>
2012-09-27 23:39:53 +0200

    Font size ubuntu fixed

--- xemacs-only.el
+++ xemacs-only.el
@@ -11,7 +11,7 @@
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
 (custom-set-faces
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(cursor ((t (:background "white")))))
 
 
