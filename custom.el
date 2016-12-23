cdd0d6a66177715b8a5ec558384c701df68e1d1d
Kuba Janoszek <kuba.janoszek@gmail.com>
2016-12-23 12:23:25 +0100

    custom: font size for ubuntu changed to 22

--- custom.el
+++ custom.el
@@ -20,7 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 200 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 220 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(cursor ((t (:background "white"))))
  '(mmm-default-submode-face ((t (:background "gray10")))))
 
