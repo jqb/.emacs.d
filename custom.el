16e946d9d3a0823a9e6fbd1f762d5502c5fd3816
jqb <kuba.janoszek@gmail.com>
2018-07-28 10:51:06 +0200

    custom.el: font size. Again.

--- custom.el
+++ custom.el
@@ -16,12 +16,14 @@
  '(tooltip-mode nil))
 
 
+;; Mono 240 is good for 4K display, but not for simple, regular monitor.
+;; With the latter I'll go with ... 170
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 220 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 170 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(cursor ((t (:background "white"))))
  '(mmm-default-submode-face ((t (:background "gray10")))))
 
@@ -48,3 +50,7 @@
      '(cursor ((t (:background "white")))))
   )
 )
+
+
+(provide 'custom)
+;;; custom.el ends here
