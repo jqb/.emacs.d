137662054ebfba89abec3c8f9a85532572b8cb75
jqb <kuba.janoszek@gmail.com>
2018-08-07 08:50:34 +0200

    init: removing non-existing snippets load-path

--- init.el
+++ init.el
@@ -598,7 +598,6 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs
       '("~/.emacs.plugins/snippets"  ;; my old snippets, as a separate repo now
-        "~/.emacs.d/elpa/yasnippet-20170923.1646/snippets" ;; different from different machines... TODO :(
         ))
 (yas/load-directory "~/.emacs.plugins/snippets")
 (yas/initialize)
